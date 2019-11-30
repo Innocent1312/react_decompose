518676d9b7bcb44e4448c59fa4cb522c96d0092a
Innocent1312 <anthonydev33@gmail.com>
2019-11-30 22:36:22 +0200

    solution

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Welcome from '../Welcome';
+import Header from '../Header';
+import Article from '../Article';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Welcome />
+      <Header />
+      <Article />
+    </>
+  );
+}
+
+export default App;
